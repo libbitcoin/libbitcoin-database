1cb431a7b9bf6ec08c232523fa42238480e6ee3d
evoskuil <eric@voskuil.org>
2016-08-11 05:29:19 -0700

    Fix spend database record_size computation (use point key vs. hash key).

--- src/databases/spend_database.cpp
+++ src/databases/spend_database.cpp
@@ -37,8 +37,8 @@ BC_CONSTEXPR size_t number_buckets = 228110589;
 BC_CONSTEXPR size_t header_size = record_hash_table_header_size(number_buckets);
 BC_CONSTEXPR size_t initial_map_file_size = header_size + minimum_records_size;
 
-BC_CONSTEXPR size_t value_size = hash_size + sizeof(uint32_t);
-BC_CONSTEXPR size_t record_size = hash_table_record_size<hash_digest>(value_size);
+BC_CONSTEXPR size_t value_size = std::tuple_size<chain::point>::value;
+BC_CONSTEXPR size_t record_size = hash_table_record_size<chain::point>(value_size);
 
 spend_database::spend_database(const path& filename,
     std::shared_ptr<shared_mutex> mutex)
