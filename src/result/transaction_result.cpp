8eec83c0dae836727d56d77eaa53c734379b6857
evoskuil <eric@voskuil.org>
2016-09-21 00:13:47 -0700

    Implement alternative tx serialization (outputs forward).

--- src/result/transaction_result.cpp
+++ src/result/transaction_result.cpp
@@ -32,9 +32,8 @@ using namespace bc::chain;
 static constexpr size_t value_size = sizeof(uint64_t);
 static constexpr size_t height_size = sizeof(uint32_t);
 static constexpr size_t version_size = sizeof(uint32_t);
-static constexpr size_t sequence_size = sizeof(uint32_t);
+static constexpr size_t locktime_size = sizeof(uint32_t);
 static constexpr size_t position_size = sizeof(uint32_t);
-static constexpr size_t outpoint_size = sizeof(hash_digest) + sizeof(uint32_t);
 
 transaction_result::transaction_result(const memory_ptr slab)
   : slab_(slab)
@@ -68,22 +67,12 @@ chain::output transaction_result::output(uint32_t index) const
     const auto tx_start = memory + height_size + position_size;
     auto serial = make_deserializer_unsafe(tx_start);
 
-    // Skip the transaction version.
-    serial.skip_bytes(version_size);
+    // THIS ASSUMES DATABASE SERIALIZATION OF TRANSACTIONS (OUTPUTS FORWARD).
 
-    // Read the number of inputs (variable).
-    const auto inputs = serial.read_variable_uint_little_endian();
+    // Skip the transaction version and locktime.
+    serial.skip_bytes(version_size + locktime_size);
 
-    // Skip all inputs (it would be optimal to store outputs before inputs).
-    for (uint64_t input = 0; input < inputs; ++input)
-    {
-        serial.skip_bytes(outpoint_size);
-        const auto script_size = serial.read_variable_uint_little_endian();
-        BITCOIN_ASSERT(script_size <= max_size_t);
-        serial.skip_bytes(static_cast<size_t>(script_size) + sequence_size);
-    }
-
-    // Read the number of inputs (variable, but point-limited to max_uint32).
+    // Read the number of outputs (variable, but point-limited to max_uint32).
     const auto outputs = serial.read_variable_uint_little_endian();
     BITCOIN_ASSERT(outputs <= max_uint32);
 
@@ -113,9 +102,10 @@ chain::transaction transaction_result::transaction() const
     const auto memory = REMAP_ADDRESS(slab_);
     const auto tx_start = memory + height_size + position_size;
     auto deserial = make_deserializer_unsafe(tx_start);
-
     chain::transaction tx;
-    tx.from_data(deserial);
+
+    // Use database serialization, not satoshi (wire protocol).
+    tx.from_data(deserial, false);
     return tx;
 }
 } // namespace database
