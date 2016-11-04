f759442295f305de813ffcda0dcbee49e0d29346
evoskuil <eric@voskuil.org>
2016-11-03 22:52:52 -0700

    Run all tests.

--- libbitcoin_database_test_runner.sh
+++ libbitcoin_database_test_runner.sh
@@ -9,7 +9,7 @@
 # Define tests and options.
 #==============================================================================
 BOOST_UNIT_TEST_OPTIONS=\
-"--run_test=database_tests,hash_table_tests,structure_tests,data_base_tests "\
+"--run_test=* "\
 "--show_progress=no "\
 "--detect_memory_leak=0 "\
 "--report_level=no "\
