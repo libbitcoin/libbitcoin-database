3d277288bb09e90097f051322d51cba8d5466447
evoskuil <eric@voskuil.org>
2017-02-16 04:17:45 -0800

    Update version3 branch installer to build from version3 branches. Fix version3 branch name typo.

--- build.cmd
+++ build.cmd
@@ -20,9 +20,9 @@ IF NOT EXIST "%nuget_pkg_path%" (
   )
 )
 
-call :init libbitcoin libbitcoin master
+call :init libbitcoin libbitcoin version3
 IF %ERRORLEVEL% NEQ 0 (
-  call :failure "Initializing repository libbitcoin libbitcoin master failed."
+  call :failure "Initializing repository libbitcoin libbitcoin version3 failed."
   exit /b 1
 )
 call :bld_repo libbitcoin-database
