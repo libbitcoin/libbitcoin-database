4ab2c19ada8877d0e7ac51d1d05a58bbd2fd1f29
evoskuil <eric@voskuil.org>
2018-01-04 03:09:10 -0800

    Revise slab_hash_table tests.

--- test/primitives/slab_hash_table.cpp
+++ test/primitives/slab_hash_table.cpp
@@ -18,98 +18,85 @@
  */
 #include <boost/test/unit_test.hpp>
 
+#include <utility>
 #include <bitcoin/database.hpp>
+#include "../utility/test_map.hpp"
 #include "../utility/utility.hpp"
 
 using namespace bc;
 using namespace bc::database;
 
-#define DIRECTORY "slab_hash_table"
-BC_CONSTEXPR size_t buckets = 100;
-BC_CONSTEXPR size_t tx_size = 200;
-BC_CONSTEXPR size_t total_txs = 200;
+BOOST_AUTO_TEST_SUITE(slab_hash_table_tests)
 
-struct slab_hash_table_directory_setup_fixture
+BOOST_AUTO_TEST_CASE(slab_hash_table__store_find__three_bytes__success)
 {
-    slab_hash_table_directory_setup_fixture()
-    {
-        BOOST_REQUIRE(test::clear_path(DIRECTORY));
-    }
-};
+    const auto buckets = 100u;
 
-BOOST_FIXTURE_TEST_SUITE(slab_hash_table_tests, slab_hash_table_directory_setup_fixture)
-
-BOOST_AUTO_TEST_CASE(slab_hash_table__write_read__test)
-{
-    // Create the data file to be read below.
-    const auto path = DIRECTORY "/slab_hash_table__write_read";
-    test::create_database_file(path, buckets, total_txs, tx_size);
-
-    file_map file(path);
+    // Open/close storage and manage flush/remap locking externaly.
+    test::test_map file;
     BOOST_REQUIRE(file.open());
-    BOOST_REQUIRE(file.access()->buffer() != nullptr);
 
+    // Create and initialize header bucket count and empty buckets.
     slab_hash_table_header header(file, buckets);
-    BOOST_REQUIRE(header.start());
-    BOOST_REQUIRE(header.size() == buckets);
-
-    const auto slab_start = slab_hash_table_header_size(buckets);
-
-    slab_manager maanager(file, slab_start);
-    BOOST_REQUIRE(maanager.start());
-
-    slab_hash_table<hash_digest> table(header, maanager);
-
-    std::default_random_engine engine;
-    for (size_t i = 0; i < total_txs; ++i)
-    {
-        const auto value = test::generate_random_bytes(engine, tx_size);
-        const auto key = bitcoin_hash(value);
-        const auto memory = table.find(key);
-        const auto slab = memory->buffer();
-
-        BOOST_REQUIRE(slab);
-        BOOST_REQUIRE(std::equal(value.begin(), value.end(), slab));
-    }
-}
-
-BOOST_AUTO_TEST_CASE(slab_hash_table__test)
-{
-    const auto path = DIRECTORY "/slab_hash_table";
-    test::create(path);
-    file_map file(path);
-    BOOST_REQUIRE(file.open());
-    BOOST_REQUIRE(file.access()->buffer() != nullptr);
-    file.resize(4 + 8 * 100 + 8);
-
-    slab_hash_table_header header(file, 100);
     BOOST_REQUIRE(header.create());
-    BOOST_REQUIRE(header.start());
 
-    slab_manager manager(file, 4 + 8 * 100);
+    // Create the initial slab (size) space after the hash table header.
+    slab_manager manager(file, slab_hash_table_header_size(buckets));
     BOOST_REQUIRE(manager.create());
-    BOOST_REQUIRE(manager.start());
 
+    // Join header and slab manager into a hash table.
     slab_hash_table<test::tiny_hash> table(header, manager);
-    const auto write = [](byte_serializer& serial)
+
+    const auto writer = [](byte_serializer& serial)
     {
-        serial.write_byte(110);
         serial.write_byte(110);
         serial.write_byte(4);
         serial.write_byte(99);
     };
-    table.store(test::tiny_hash{ { 0xde, 0xad, 0xbe, 0xef } }, write, 8);
-    const auto memory1 = table.find(test::tiny_hash{ { 0xde, 0xad, 0xbe, 0xef } });
-    const auto slab1 = memory1->buffer();
-    BOOST_REQUIRE(slab1);
-    BOOST_REQUIRE(slab1[0] == 110);
-    BOOST_REQUIRE(slab1[1] == 110);
-    BOOST_REQUIRE(slab1[2] == 4);
-    BOOST_REQUIRE(slab1[3] == 99);
+    const test::tiny_hash key{ { 0xde, 0xad, 0xbe, 0xef } };
+    table.store(key, writer, 3);
+    const auto memory = table.find(key);
+    const auto slab = memory->buffer();
+    BOOST_REQUIRE(slab);
+    BOOST_REQUIRE_EQUAL(slab[0], 110u);
+    BOOST_REQUIRE_EQUAL(slab[1], 4u);
+    BOOST_REQUIRE_EQUAL(slab[2], 99u);
+}
 
-    const auto memory2 = table.find(test::tiny_hash{ { 0xde, 0xad, 0xbe, 0xee } });
-    const auto slab2 = memory1->buffer();
-    BOOST_REQUIRE(slab2);
+BOOST_AUTO_TEST_CASE(slab_hash_table__store_find__random_bytes_multiple_times__test)
+{
+    const auto buckets = 100u;
+
+    // Open/close storage and manage flush/remap locking externaly.
+    test::test_map file;
+    BOOST_REQUIRE(file.open());
+
+    // Create and initialize header bucket count and empty buckets.
+    slab_hash_table_header header(file, buckets);
+    BOOST_REQUIRE(header.create());
+
+    // Create the initial slab (size) space after the hash table header.
+    slab_manager manager(file, slab_hash_table_header_size(buckets));
+    BOOST_REQUIRE(manager.create());
+
+    // Join header and slab manager into a hash table.
+    slab_hash_table<hash_digest> table(header, manager);
+
+    std::default_random_engine engine;
+    for (size_t i = 0; i < 50; ++i)
+    {
+        const auto value = test::generate_random_bytes(engine, 200);
+        const auto key = bitcoin_hash(value);
+        const auto writer = [&](byte_serializer& serial)
+        {
+            serial.write_bytes(value);
+        };
+        table.store(key, writer, value.size());
+        const auto memory = table.find(key);
+        const auto slab = memory->buffer();
+        BOOST_REQUIRE(slab);
+        BOOST_REQUIRE(std::equal(value.begin(), value.end(), slab));
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
