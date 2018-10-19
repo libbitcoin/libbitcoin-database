b3baccdebdc8c658a134ddd20048d6b214fe71f9
Kulpreet Singh <zapfmann@gmail.com>
2018-10-20 00:14:27 +0200

    Inital pass at refactoring transaction database tests

--- test/databases/transaction_database.cpp
+++ test/databases/transaction_database.cpp
@@ -28,61 +28,97 @@ using namespace bc;
 using namespace bc::chain;
 using namespace bc::database;
 
-#define DIRECTORY "transaction_database"
-#define TRANSACTION1 "0100000001537c9d05b5f7d67b09e5108e3bd5e466909cc9403ddd98bc42973f366fe729410600000000ffffffff0163000000000000001976a914fe06e7b4c88a719e92373de489c08244aee4520b88ac00000000"
-#define TRANSACTION2 "010000000147811c3fc0c0e750af5d0ea7343b16ea2d0c291c002e3db778669216eb689de80000000000ffffffff0118ddf505000000001976a914575c2f0ea88fcbad2389a372d942dea95addc25b88ac00000000"
-
-struct transaction_database_directory_setup_fixture
+struct transaction_database_fixture
 {
-    transaction_database_directory_setup_fixture()
+    const std::string directory = "transaction_database";
+    const std::string path = "/tx_table";
+    const std::string transaction1 = "0100000001537c9d05b5f7d67b09e5108e3bd5e466909cc9403ddd98bc42973f366fe729410600000000ffffffff0163000000000000001976a914fe06e7b4c88a719e92373de489c08244aee4520b88ac00000000";
+    const std::string transaction2 = "010000000147811c3fc0c0e750af5d0ea7343b16ea2d0c291c002e3db778669216eb689de80000000000ffffffff0118ddf505000000001976a914575c2f0ea88fcbad2389a372d942dea95addc25b88ac00000000";
+
+    std::unique_ptr<transaction_database> db;
+    
+    transaction tx1;
+    data_chunk wire_tx1;
+    hash_digest hash1;
+
+    transaction tx2;
+    data_chunk wire_tx2;    
+    hash_digest hash2;
+
+    transaction::list all_transactions;
+    
+    transaction_database_fixture()
     {
-        test::clear_path(DIRECTORY);
+        test::clear_path(directory);
+        test::create(directory + path);
+
+        db = std::unique_ptr<transaction_database>{new transaction_database(directory + path, 1000, 50, 0)};
+        BOOST_REQUIRE(db->create());
+
+        BOOST_REQUIRE(decode_base16(wire_tx1, transaction1));
+        BOOST_REQUIRE(tx1.from_data(wire_tx1));
+        all_transactions.push_back(tx1);
+        
+        BOOST_REQUIRE(decode_base16(wire_tx2, transaction2));
+        BOOST_REQUIRE(tx2.from_data(wire_tx2));
+        all_transactions.push_back(tx2);
+        
+        hash1 = tx1.hash();        
+        hash2 = tx2.hash();
+        
+        BOOST_REQUIRE(!db->get(hash1));
+        BOOST_REQUIRE(!db->get(hash2));
     }
+
+    ~transaction_database_fixture()
+    {
+        test::clear_path(directory);
+    }
+    
 };
 
-BOOST_FIXTURE_TEST_SUITE(database_tests, transaction_database_directory_setup_fixture)
+BOOST_FIXTURE_TEST_SUITE(transaction_database_tests, transaction_database_fixture)
 
-// TODO: reimplement.
-BOOST_AUTO_TEST_CASE(transaction_database__test)
+BOOST_AUTO_TEST_CASE(transaction_database__store_and_get__single_transactions__succeeded)
 {
-    ////transaction tx1;
-    ////data_chunk wire_tx1;
-    ////BOOST_REQUIRE(decode_base16(wire_tx1, TRANSACTION1));
-    ////BOOST_REQUIRE(tx1.from_data(wire_tx1));
+    db->store({tx1}, 110, 100);
 
-    ////transaction tx2;
-    ////data_chunk wire_tx2;
-    ////BOOST_REQUIRE(decode_base16(wire_tx2, TRANSACTION2));
-    ////BOOST_REQUIRE(tx2.from_data(wire_tx2));
+    const auto result1 = db->get(hash1);
+    BOOST_REQUIRE(result1);
+    BOOST_REQUIRE(result1.transaction().hash() == hash1);
 
-    ////const auto path = DIRECTORY "/tx_table";
-    ////test::create(path);
-    ////transaction_database db(path, 1000, 50, 0);
-    ////BOOST_REQUIRE(db.create());
+    db->store({tx2}, 220, 200);
 
-    ////const auto hash1 = tx1.hash();
-    ////BOOST_REQUIRE(!db.get(hash1));
+    const auto result2 = db->get(hash2);
+    BOOST_REQUIRE(result2);
+    BOOST_REQUIRE(result2.transaction().hash() == hash2);
 
-    ////const auto hash2 = tx2.hash();
-    ////BOOST_REQUIRE(!db.get(hash2));
+    // Verify get by link in tx metadata
+    const auto result3 = db->get(result2.link());
+    BOOST_REQUIRE(result3);
+    BOOST_REQUIRE(result3.transaction().hash() == hash2);
 
-    ////db.store(tx1, 110, 0, 88, false);
-    ////db.store(tx2, 4, 0, 6, false);
+    db->commit();
+}
 
-    ////const auto result1 = db.get(hash1);
-    ////BOOST_REQUIRE(result1);
-    ////BOOST_REQUIRE(result1.transaction().hash() == hash1);
+BOOST_AUTO_TEST_CASE(transaction_database__store_and_get__transaction_list__succeeded)
+{
+    db->store({tx1, tx2}, 110, 100);
 
-    ////const auto result2 = db.get(hash2);
-    ////BOOST_REQUIRE(result2);
-    ////BOOST_REQUIRE(result2.transaction().hash() == hash2);
+    const auto result1 = db->get(hash1);
+    BOOST_REQUIRE(result1);
+    BOOST_REQUIRE(result1.transaction().hash() == hash1);
 
-    ////// Verify computed hash.
-    ////const auto result3 = db.get(result2.link());
-    ////BOOST_REQUIRE(result3);
-    ////BOOST_REQUIRE(result3.transaction().hash() == hash2);
+    const auto result2 = db->get(hash2);
+    BOOST_REQUIRE(result2);
+    BOOST_REQUIRE(result2.transaction().hash() == hash2);
 
-    ////db.commit();
+    // Verify get by link in tx metadata
+    const auto result3 = db->get(result2.link());
+    BOOST_REQUIRE(result3);
+    BOOST_REQUIRE(result3.transaction().hash() == hash2);
+
+    db->commit();
 }
 
 BOOST_AUTO_TEST_SUITE_END()
