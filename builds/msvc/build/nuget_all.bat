7f297a50da2432bad6478307d8fa0951b1637f26
evoskuil <eric@voskuil.org>
2022-02-26 22:24:27 -0800

    Regenerate artifacts, add vs2019/2022.

--- builds/msvc/build/nuget_all.bat
+++ builds/msvc/build/nuget_all.bat
@@ -1,15 +1,15 @@
 @ECHO OFF
+ECHO Downloading libbitcoin vs2022 dependencies from NuGet
+CALL nuget.exe install ..\vs2022\libbitcoin-database\packages.config
+CALL nuget.exe install ..\vs2022\libbitcoin-database-tools\packages.config
+CALL nuget.exe install ..\vs2022\libbitcoin-database-test\packages.config
+ECHO.
+ECHO Downloading libbitcoin vs2019 dependencies from NuGet
+CALL nuget.exe install ..\vs2019\libbitcoin-database\packages.config
+CALL nuget.exe install ..\vs2019\libbitcoin-database-tools\packages.config
+CALL nuget.exe install ..\vs2019\libbitcoin-database-test\packages.config
+ECHO.
 ECHO Downloading libbitcoin vs2017 dependencies from NuGet
 CALL nuget.exe install ..\vs2017\libbitcoin-database\packages.config
 CALL nuget.exe install ..\vs2017\libbitcoin-database-tools\packages.config
 CALL nuget.exe install ..\vs2017\libbitcoin-database-test\packages.config
-ECHO.
-ECHO Downloading libbitcoin vs2015 dependencies from NuGet
-CALL nuget.exe install ..\vs2015\libbitcoin-database\packages.config
-CALL nuget.exe install ..\vs2015\libbitcoin-database-tools\packages.config
-CALL nuget.exe install ..\vs2015\libbitcoin-database-test\packages.config
-ECHO.
-ECHO Downloading libbitcoin vs2013 dependencies from NuGet
-CALL nuget.exe install ..\vs2013\libbitcoin-database\packages.config
-CALL nuget.exe install ..\vs2013\libbitcoin-database-tools\packages.config
-CALL nuget.exe install ..\vs2013\libbitcoin-database-test\packages.config
