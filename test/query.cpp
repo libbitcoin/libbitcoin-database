826732558b624d47f9cdf6fc3ee2f5a86a207ce5
evoskuil <eric@voskuil.org>
2022-12-19 12:57:42 -0800

    Re-enable mmap query test.

--- test/query.cpp
+++ test/query.cpp
@@ -40,8 +40,7 @@ struct query_setup_fixture
 BOOST_FIXTURE_TEST_SUITE(query_tests, query_setup_fixture)
 
 using query_accessor = query<store<test::chunk_storage>>;
-
-constexpr auto parent = system::null_hash;
+const auto genesis = system::settings{ system::chain::selection::mainnet }.genesis_block;
 constexpr database::context context
 {
     0x11121314, // flags
@@ -51,6 +50,53 @@ constexpr database::context context
 
 // chain::header
 
+// slow test (mmap)
+BOOST_AUTO_TEST_CASE(query__set_header__mmap_get_header__expected)
+{
+    constexpr auto parent = system::null_hash;
+    constexpr auto merkle_root = system::base16_array("119192939495969798999a9b9c9d9e9f229192939495969798999a9b9c9d9e9f");
+    constexpr auto block_hash = system::base16_array("85d0b02a16f6d645aa865fad4a8666f5e7bb2b0c4392a5d675496d6c3defa1f2");
+    const system::chain::header header
+    {
+        0x31323334, // version
+        parent,     // previous_block_hash
+        merkle_root,// merkle_root
+        0x41424344, // timestamp
+        0x51525354, // bits
+        0x61626364  // nonce
+    };
+
+    settings settings1{};
+    settings1.header_buckets = 10;
+    settings1.dir = TEST_DIRECTORY;
+    store<map> store1{ settings1 };
+    query<store<map>> query1{ store1 };
+    BOOST_REQUIRE_EQUAL(store1.create(), error::success);
+    BOOST_REQUIRE_EQUAL(store1.open(), error::success);
+    {
+        const auto transactor = store1.get_transactor();
+        BOOST_REQUIRE(query1.set(header, context));
+    }
+    table::header::record element1{};
+    BOOST_REQUIRE(store1.header.get(query1.to_header(block_hash), element1));
+
+    const auto pointer = query1.get_header(query1.to_header(block_hash));
+    BOOST_REQUIRE(pointer);
+    BOOST_REQUIRE(*pointer == header);
+
+    BOOST_REQUIRE_EQUAL(store1.close(), error::success);
+
+    BOOST_REQUIRE_EQUAL(element1.ctx.height, system::mask_left(context.height, byte_bits));
+    BOOST_REQUIRE_EQUAL(element1.ctx.flags, context.flags);
+    BOOST_REQUIRE_EQUAL(element1.ctx.mtp, context.mtp);
+    BOOST_REQUIRE_EQUAL(element1.version, header.version());
+    BOOST_REQUIRE_EQUAL(element1.parent_fk, linkage<schema::header::pk>::terminal);
+    BOOST_REQUIRE_EQUAL(element1.merkle_root, header.merkle_root());
+    BOOST_REQUIRE_EQUAL(element1.timestamp, header.timestamp());
+    BOOST_REQUIRE_EQUAL(element1.bits, header.bits());
+    BOOST_REQUIRE_EQUAL(element1.nonce, header.nonce());
+}
+
 BOOST_AUTO_TEST_CASE(query__set_header__default__expected)
 {
     constexpr auto merkle_root = system::base16_array("119192939495969798999a9b9c9d9e9f229192939495969798999a9b9c9d9e9f");
@@ -58,7 +104,7 @@ BOOST_AUTO_TEST_CASE(query__set_header__default__expected)
     const system::chain::header header
     {
         0x31323334, // version
-        parent,     // previous_block_hash
+        system::null_hash, // previous_block_hash
         merkle_root,// merkle_root
         0x41424344, // timestamp
         0x51525354, // bits
@@ -124,7 +170,7 @@ BOOST_AUTO_TEST_CASE(query__get_header__default__expected)
     const system::chain::header header
     {
         0x31323334, // version
-        parent,     // previous_block_hash
+        system::null_hash, // previous_block_hash
         root,       // merkle_root
         0x41424344, // timestamp
         0x51525354, // bits
@@ -169,53 +215,6 @@ BOOST_AUTO_TEST_CASE(query__get_header__default__expected)
     BOOST_REQUIRE(pointer1);
     BOOST_REQUIRE(*pointer1 == header);
 }
-
-////// slow test (mmap)
-////BOOST_AUTO_TEST_CASE(query__set_header__mmap_get_header__expected)
-////{
-////    constexpr auto parent = system::null_hash;
-////    constexpr auto merkle_root = system::base16_array("119192939495969798999a9b9c9d9e9f229192939495969798999a9b9c9d9e9f");
-////    constexpr auto block_hash = system::base16_array("85d0b02a16f6d645aa865fad4a8666f5e7bb2b0c4392a5d675496d6c3defa1f2");
-////    const system::chain::header header
-////    {
-////        0x31323334, // version
-////        parent,     // previous_block_hash
-////        merkle_root,// merkle_root
-////        0x41424344, // timestamp
-////        0x51525354, // bits
-////        0x61626364  // nonce
-////    };
-////
-////    settings settings1{};
-////    settings1.header_buckets = 10;
-////    settings1.dir = TEST_DIRECTORY;
-////    store<map> store1{ settings1 };
-////    query<store<map>> query1{ store1 };
-////    BOOST_REQUIRE_EQUAL(store1.create(), error::success);
-////    BOOST_REQUIRE_EQUAL(store1.open(), error::success);
-////    {
-////        const auto transactor = store1.get_transactor();
-////        BOOST_REQUIRE(query1.set_header(header, context));
-////    }
-////    table::header::record element1{};
-////    BOOST_REQUIRE(store1.header.get(block_hash, element1));
-////
-////    const auto pointer = query1.get_header(block_hash);
-////    BOOST_REQUIRE(pointer);
-////    BOOST_REQUIRE(*pointer == header);
-////
-////    BOOST_REQUIRE_EQUAL(store1.close(), error::success);
-////
-////    BOOST_REQUIRE_EQUAL(element1.ctx.height, system::mask_left(context.height, byte_bits));
-////    BOOST_REQUIRE_EQUAL(element1.ctx.flags, context.flags);
-////    BOOST_REQUIRE_EQUAL(element1.ctx.mtp, context.mtp);
-////    BOOST_REQUIRE_EQUAL(element1.version, header.version());
-////    BOOST_REQUIRE_EQUAL(element1.parent_fk, linkage<schema::header::pk>::terminal);
-////    BOOST_REQUIRE_EQUAL(element1.merkle_root, header.merkle_root());
-////    BOOST_REQUIRE_EQUAL(element1.timestamp, header.timestamp());
-////    BOOST_REQUIRE_EQUAL(element1.bits, header.bits());
-////    BOOST_REQUIRE_EQUAL(element1.nonce, header.nonce());
-////}
 
 // chain::transaction
 
@@ -510,98 +509,97 @@ BOOST_AUTO_TEST_CASE(query__set_tx__get_tx__expected)
 
 // chain::block
 
-const auto genesis = system::settings{ system::chain::selection::mainnet }.genesis_block;
-const auto genesis_header_head = system::base16_chunk(
-    "010000"       // record count
-    "ffffff"       // bucket[0]...
-    "000000"       // pk->
-    "ffffff"
-    "ffffff"
-    "ffffff");
-const auto genesis_header_body = system::base16_chunk(
-    "ffffff"       // next->
-    "6fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000" // sk (block.hash)
-    "14131211"     // flags
-    "040302"       // height
-    "24232221"     // mtp
-    "ffffff"       // previous_block_hash (header_fk - not found)
-    "01000000"     // version
-    "29ab5f49"     // timestamp
-    "ffff001d"     // bits
-    "1dac2b7c"     // nonce
-    "3ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a"); // merkle_root
-const auto genesis_tx_head = system::base16_chunk(
-    "01000000"     // record count
-    "ffffffff"     // bucket[0]...
-    "ffffffff"
-    "ffffffff"
-    "00000000"     // pk->
-    "ffffffff");
-const auto genesis_tx_body = system::base16_chunk(
-    "ffffffff"     // next->
-    "3ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a" // sk (tx.hash(false))
-    "01"           // coinbase
-    "cc0000"       // witless
-    "cc0000"       // witness
-    "00000000"     // locktime
-    "01000000"     // version
-    "010000"       // ins_count
-    "010000"       // outs_count
-    "00000000");   // puts_fk->
-const auto genesis_puts_head = system::base16_chunk("02000000");
-const auto genesis_puts_body = system::base16_chunk(
-    "0000000000"   // input0_fk->
-    "0000000000"); // output0_fk->
-const auto genesis_output_head = system::base16_chunk("5200000000");
-const auto genesis_output_body = system::base16_chunk(
-    "00000000"     // parent_fk->
-    "00"           // index
-    "ff00f2052a01000000" // value
-    "434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac"); // script
-const auto genesis_point_head = system::base16_chunk(
-    "00000000"     // record count (null point, empty)
-    "ffffffff"
-    "ffffffff"
-    "ffffffff"
-    "ffffffff"
-    "ffffffff");
-const auto genesis_point_body = system::base16_chunk("");
-const auto genesis_input_head = system::base16_chunk(
-    "6400000000"   // slabs size
-    "ffffffffff"
-    "ffffffffff"
-    "0000000000"   // pk->[0]
-    "ffffffffff"
-    "ffffffffff");
-const auto genesis_input_body = system::base16_chunk(
-    "ffffffffff"   // next->
-    "ffffffff"     // sk (point_fk)
-    "ffffff"       // sk (point.index) [this tests 4 bytes null_index recovery]
-    "00000000"     // parent_fk->
-    "00"           // index
-    "ffffffff"     // sequence
-    "4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73" // script
-    "00");         // witness
-const auto genesis_txs_head = system::base16_chunk(
-    "0f000000"     // slabs size
-    "ffffffff"
-    "ffffffff"
-    "ffffffff"
-    "ffffffff"
-    "ffffffff"
-    "ffffffff"
-    "ffffffff"
-    "00000000"      // pk->
-    "ffffffff"
-    "ffffffff");
-const auto genesis_txs_body = system::base16_chunk(
-    "ffffffff"      // next->
-    "000000"        // header_fk
-    "01000000"      // txs count (1)
-    "00000000");    // transaction[0]
-
 BOOST_AUTO_TEST_CASE(query__set_block__get_block__expected)
 {
+    const auto genesis_header_head = system::base16_chunk(
+        "010000"       // record count
+        "ffffff"       // bucket[0]...
+        "000000"       // pk->
+        "ffffff"
+        "ffffff"
+        "ffffff");
+    const auto genesis_header_body = system::base16_chunk(
+        "ffffff"       // next->
+        "6fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000" // sk (block.hash)
+        "14131211"     // flags
+        "040302"       // height
+        "24232221"     // mtp
+        "ffffff"       // previous_block_hash (header_fk - not found)
+        "01000000"     // version
+        "29ab5f49"     // timestamp
+        "ffff001d"     // bits
+        "1dac2b7c"     // nonce
+        "3ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a"); // merkle_root
+    const auto genesis_tx_head = system::base16_chunk(
+        "01000000"     // record count
+        "ffffffff"     // bucket[0]...
+        "ffffffff"
+        "ffffffff"
+        "00000000"     // pk->
+        "ffffffff");
+    const auto genesis_tx_body = system::base16_chunk(
+        "ffffffff"     // next->
+        "3ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a" // sk (tx.hash(false))
+        "01"           // coinbase
+        "cc0000"       // witless
+        "cc0000"       // witness
+        "00000000"     // locktime
+        "01000000"     // version
+        "010000"       // ins_count
+        "010000"       // outs_count
+        "00000000");   // puts_fk->
+    const auto genesis_puts_head = system::base16_chunk("02000000");
+    const auto genesis_puts_body = system::base16_chunk(
+        "0000000000"   // input0_fk->
+        "0000000000"); // output0_fk->
+    const auto genesis_output_head = system::base16_chunk("5200000000");
+    const auto genesis_output_body = system::base16_chunk(
+        "00000000"     // parent_fk->
+        "00"           // index
+        "ff00f2052a01000000" // value
+        "434104678afdb0fe5548271967f1a67130b7105cd6a828e03909a67962e0ea1f61deb649f6bc3f4cef38c4f35504e51ec112de5c384df7ba0b8d578a4c702b6bf11d5fac"); // script
+    const auto genesis_point_head = system::base16_chunk(
+        "00000000"     // record count (null point, empty)
+        "ffffffff"
+        "ffffffff"
+        "ffffffff"
+        "ffffffff"
+        "ffffffff");
+    const auto genesis_point_body = system::base16_chunk("");
+    const auto genesis_input_head = system::base16_chunk(
+        "6400000000"   // slabs size
+        "ffffffffff"
+        "ffffffffff"
+        "0000000000"   // pk->[0]
+        "ffffffffff"
+        "ffffffffff");
+    const auto genesis_input_body = system::base16_chunk(
+        "ffffffffff"   // next->
+        "ffffffff"     // sk (point_fk)
+        "ffffff"       // sk (point.index) [this tests 4 bytes null_index recovery]
+        "00000000"     // parent_fk->
+        "00"           // index
+        "ffffffff"     // sequence
+        "4d04ffff001d0104455468652054696d65732030332f4a616e2f32303039204368616e63656c6c6f72206f6e206272696e6b206f66207365636f6e64206261696c6f757420666f722062616e6b73" // script
+        "00");         // witness
+    const auto genesis_txs_head = system::base16_chunk(
+        "0f000000"     // slabs size
+        "ffffffff"
+        "ffffffff"
+        "ffffffff"
+        "ffffffff"
+        "ffffffff"
+        "ffffffff"
+        "ffffffff"
+        "00000000"      // pk->
+        "ffffffff"
+        "ffffffff");
+    const auto genesis_txs_body = system::base16_chunk(
+        "ffffffff"      // next->
+        "000000"        // header_fk
+        "01000000"      // txs count (1)
+        "00000000");    // transaction[0]
+
     settings settings1{};
     settings1.header_buckets = 5;
     settings1.tx_buckets = 5;
