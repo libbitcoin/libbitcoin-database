529d06ad8788a251dd2cc941eb8fcd82fa3e90eb
evoskuil <eric@voskuil.org>
2023-01-01 00:17:18 -0800

    Stub in validation tests, expand coverage.

---
+++ test/query/validation.cpp
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2011-2022 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "../test.hpp"
+#include "../mocks/blocks.hpp"
+#include "../mocks/chunk_store.hpp"
+
+struct query_validation_setup_fixture
+{
+    DELETE4(query_validation_setup_fixture);
+    BC_PUSH_WARNING(NO_THROW_IN_NOEXCEPT)
+
+    query_validation_setup_fixture() NOEXCEPT
+    {
+        BOOST_REQUIRE(test::clear(test::directory));
+    }
+
+    ~query_validation_setup_fixture() NOEXCEPT
+    {
+        BOOST_REQUIRE(test::clear(test::directory));
+    }
+
+    BC_POP_WARNING()
+};
+
+BOOST_FIXTURE_TEST_SUITE(query_validation_tests, query_validation_setup_fixture)
+
+BOOST_AUTO_TEST_CASE(query_validation_test)
+{
+    BOOST_REQUIRE(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
