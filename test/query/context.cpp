388a8958da1a734ee0de41c1d041ad4a6b4e7975
evoskuil <eric@voskuil.org>
2023-03-28 03:25:44 -0700

    Enhance and add context tests.

--- test/query/context.cpp
+++ test/query/context.cpp
@@ -44,7 +44,17 @@ const auto events = [](auto, auto) {};
 
 BOOST_AUTO_TEST_CASE(query_context__get_chain_state__genesis__expected)
 {
-    const system::settings system_settings{};
+    const system::settings system_settings{ system::chain::selection::mainnet };
+    const system::chain::context expected
+    {
+        131203u,
+        test::genesis.header().timestamp(),
+        0u,
+        0u,
+        1u,
+        0u
+    };
+
     database::settings database_settings{};
     database_settings.path = TEST_DIRECTORY;
     test::chunk_store store{ database_settings };
@@ -54,7 +64,54 @@ BOOST_AUTO_TEST_CASE(query_context__get_chain_state__genesis__expected)
 
     const auto state = query.get_chain_state(system_settings);
     BOOST_REQUIRE(state);
-    BOOST_REQUIRE_EQUAL(state->height(), zero);
+    BOOST_REQUIRE_EQUAL(state->forks(), expected.forks);
+    BOOST_REQUIRE_EQUAL(state->height(), expected.height);
+    BOOST_REQUIRE_EQUAL(state->timestamp(), expected.timestamp);
+    BOOST_REQUIRE_EQUAL(state->work_required(), expected.work_required);
+    BOOST_REQUIRE_EQUAL(state->median_time_past(), expected.median_time_past);
+    BOOST_REQUIRE_EQUAL(state->minimum_block_version(), expected.minimum_block_version);
+    BOOST_REQUIRE(state->context() == expected);
+}
+
+BOOST_AUTO_TEST_CASE(query_context__get_chain_state__block1__expected)
+{
+    const system::settings system_settings{ system::chain::selection::mainnet };
+    const system::chain::context expected
+    {
+        131203u,                            // forks
+        test::block1.header().timestamp(),  // timestamp
+        test::genesis.header().timestamp(), // mtp
+        1u,                                 // height
+        1u,                                 // minimum_block_version
+        486604799u                          // work_required
+    };
+
+    // Not actually contributory.
+    const database::context context
+    {
+        expected.forks,
+        static_cast<uint32_t>(expected.height),
+        expected.median_time_past
+    };
+
+    database::settings database_settings{};
+    database_settings.path = TEST_DIRECTORY;
+    test::chunk_store store{ database_settings };
+    test::query_accessor query{ store };
+    BOOST_REQUIRE_EQUAL(store.create(events), error::success);
+    BOOST_REQUIRE(query.initialize(test::genesis));
+    BOOST_REQUIRE(query.set(test::block1, context));
+    BOOST_REQUIRE(query.push_candidate(query.to_header(test::block1.hash())));
+
+    const auto state = query.get_chain_state(system_settings);
+    BOOST_REQUIRE(state);
+    BOOST_REQUIRE_EQUAL(state->forks(), expected.forks);
+    BOOST_REQUIRE_EQUAL(state->height(), expected.height);
+    BOOST_REQUIRE_EQUAL(state->timestamp(), expected.timestamp);
+    BOOST_REQUIRE_EQUAL(state->work_required(), expected.work_required);
+    BOOST_REQUIRE_EQUAL(state->median_time_past(), expected.median_time_past);
+    BOOST_REQUIRE_EQUAL(state->minimum_block_version(), expected.minimum_block_version);
+    BOOST_REQUIRE(state->context() == expected);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
