f399b9b8b067bf3c176e42b5ebe845b86e7b6321
Phillip Mienk <mienkphi@gmail.com>
2023-08-17 13:12:23 -0700

    Regenerate with updated version to 3.8.0.

--- include/bitcoin/database/version.hpp
+++ include/bitcoin/database/version.hpp
@@ -12,9 +12,9 @@
  * For interpretation of the versioning scheme see: http://semver.org
  */
 
-#define LIBBITCOIN_DATABASE_VERSION "3.7.0"
+#define LIBBITCOIN_DATABASE_VERSION "3.8.0"
 #define LIBBITCOIN_DATABASE_MAJOR_VERSION 3
-#define LIBBITCOIN_DATABASE_MINOR_VERSION 7
+#define LIBBITCOIN_DATABASE_MINOR_VERSION 8
 #define LIBBITCOIN_DATABASE_PATCH_VERSION 0
 
 #endif
