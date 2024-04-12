ba39ded8b52d55d8eae0196f60792caf6d572f2a
evoskuil <eric@voskuil.org>
2024-04-11 20:37:57 -0400

    Remove dead comments.

--- include/bitcoin/database/query.hpp
+++ include/bitcoin/database/query.hpp
@@ -295,7 +295,6 @@ public:
 
     bool set_block_confirmable(const header_link& link, uint64_t fees) NOEXCEPT;
     bool set_block_preconfirmable(const header_link& link) NOEXCEPT;
-    ////bool set_block_malleable(const header_link& link) NOEXCEPT;
     bool set_block_unconfirmable(const header_link& link) NOEXCEPT;
 
     // set_txs_connected is FOR PERFORMANCE EVALUATION ONLY.
