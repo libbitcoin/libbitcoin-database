ba0d012923d4589ba7d9f0096b84831f70b40c39
evoskuil <eric@voskuil.org>
2024-05-15 15:58:56 -0400

    Add tx/txs archive error codes.

--- src/error.cpp
+++ src/error.cpp
@@ -96,7 +96,26 @@ DEFINE_ERROR_T_MESSAGE_MAP(error)
     { unspent_coinbase_collision, "unspent coinbase collision" },
     { relative_time_locked, "relative time locked" },
     { unconfirmed_spend, "unconfirmed spend" },
-    { confirmed_double_spend, "confirmed double spend" }
+    { confirmed_double_spend, "confirmed double spend" },
+
+    // tx archive
+    { tx_empty, "tx_empty" },
+    { tx_tx_allocate, "tx_tx_allocate" },
+    { tx_spend_allocate, "tx_spend_allocate" },
+    { tx_input_put, "tx_input_put" },
+    { tx_point_put, "tx_point_put" },
+    { tx_spend_set, "tx_spend_set" },
+    { tx_output_put, "tx_output_put" },
+    { tx_tx_set, "tx_tx_set" },
+    { tx_puts_put, "tx_puts_put" },
+    { tx_spend_commit, "tx_spend_commit" },
+    { tx_address_put, "tx_address_put" },
+    { tx_tx_commit, "tx_tx_commit" },
+
+    // tx archive
+    { txs_header, "txs_header" },
+    { txs_tx, "txs_tx" },
+    { txs_txs_put, "txs_txs_put" }
 };
 
 DEFINE_ERROR_T_CATEGORY(error, "database", "database code")
