libbitcoin/libbitcoin-database

commit f5c7406909c96db2f5bbeb435b2c25ccd70cd675

Implement minimize setting.

include/bitcoin/database/impl/query/archive.ipp
@@ -731,23 +731,25 @@ code CLASS::set_code(tx_link& out_fk, const transaction& tx) NOEXCEPT
         point_link hash_fk{};
         if (hash != null_hash)
         {
-            // TODO: look into point table removal or conditional duplicates.
-            // TODO: allowing duplication here increases store by 45GiB.
-
-            ////// GUARD (tx redundancy)
-            ////// Only fully effective if there is a single database thread.
-            ////hash_fk = to_point(hash);
-            ////if (hash_fk.is_terminal())
-            ////{
-            // Safe allocation failure, duplicates [limited but] expected.
-            if (!store_.point.put_link(hash_fk, hash, table::point::record
+            // TODO: look into point table removal.
+            if (minimize_)
             {
-                // Table stores no data other than the search key.
-            }))
-            {
-                return error::tx_point_put;
+                // GUARD (tx redundancy)
+                // Only fully effective if there is a single database thread.
+                // This reduces point store by ~45GiB, but causes thrashing.
+                hash_fk = to_point(hash);
+                if (hash_fk.is_terminal())
+                {
+                    // Safe allocation failure, duplicates limited but expected.
+                    if (!store_.point.put_link(hash_fk, hash, table::point::record
+                    {
+                        // Table stores no data other than the search key.
+                    }))
+                    {
+                        return error::tx_point_put;
+                    }
+                }
             }
-            ////}
         }
 
         // Accumulate spend keys in order (terminal for any null point).

include/bitcoin/database/impl/query/query.ipp
@@ -69,8 +69,8 @@ namespace libbitcoin {
 namespace database {
 
 TEMPLATE
-CLASS::query(Store& value) NOEXCEPT
-  : store_(value)
+CLASS::query(Store& store) NOEXCEPT
+  : store_(store), minimize_(store.minimize())
 {
 }
 

include/bitcoin/database/impl/store.ipp
@@ -1115,6 +1115,12 @@ void CLASS::report(const error_handler& handler) const NOEXCEPT
     ////report(buffer_body_, table_t::buffer_body);
 }
 
+TEMPLATE
+bool CLASS::minimize() const NOEXCEPT
+{
+    return configuration_.minimize;
+}
+
 BC_POP_WARNING()
 
 } // namespace database

include/bitcoin/database/query.hpp
@@ -81,7 +81,7 @@ public:
     using heights = std_vector<size_t>;
     using filter = system::data_chunk;
 
-    query(Store& value) NOEXCEPT;
+    query(Store& store) NOEXCEPT;
 
     /// Store management from query-holder (not store owner) context.
     /// -----------------------------------------------------------------------
@@ -537,7 +537,9 @@ private:
     static inline bool contains(const block_txs& blocks,
         const block_tx& block) NOEXCEPT;
 
+    // These are thread safe.
     Store& store_;
+    bool minimize_;
 };
 
 } // namespace database

include/bitcoin/database/store.hpp
@@ -91,6 +91,9 @@ public:
     /// Dump all error/full conditions to handler.
     void report(const error_handler& handler) const NOEXCEPT;
 
+    /// Favor minimum size over thrashing guard (requires high memory).
+    bool minimize() const NOEXCEPT;
+
     /// Tables.
     /// -----------------------------------------------------------------------
 

commit aa202eb3cb203643d8c50f94a3a3acab16301c3a

Fix empty point table regression introduced in f5c74069.

include/bitcoin/database/impl/query/archive.ipp
@@ -727,27 +727,25 @@ code CLASS::set_code(tx_link& out_fk, const transaction& tx) NOEXCEPT
         const auto& prevout = in->point();
         const auto& hash = prevout.hash();
 
-        // Get or create prevout hash in point table (reduces duplicates).
+        // TODO: consider table removal.
+        // Create prevout hash in point table.
         point_link hash_fk{};
         if (hash != null_hash)
         {
-            // TODO: look into point table removal.
-            if (minimize_)
+            // GUARD (tx redundancy)
+            // Only fully effective if there is a single database thread.
+            // This reduces point store by ~45GiB, but causes thrashing.
+            if (minimize_) hash_fk = to_point(hash);
+
+            if (hash_fk.is_terminal())
             {
-                // GUARD (tx redundancy)
-                // Only fully effective if there is a single database thread.
-                // This reduces point store by ~45GiB, but causes thrashing.
-                hash_fk = to_point(hash);
-                if (hash_fk.is_terminal())
+                // Safe allocation failure, duplicates limited but expected.
+                if (!store_.point.put_link(hash_fk, hash, table::point::record
                 {
-                    // Safe allocation failure, duplicates limited but expected.
-                    if (!store_.point.put_link(hash_fk, hash, table::point::record
-                    {
-                        // Table stores no data other than the search key.
-                    }))
-                    {
-                        return error::tx_point_put;
-                    }
+                    // Table stores no data other than the search key.
+                }))
+                {
+                    return error::tx_point_put;
                 }
             }
         }