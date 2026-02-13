98b9148e3c6c62e1072b2fe90900619ac7e7c2b6
evoskuil <eric@voskuil.org>
2026-02-12 21:03:01 -0500

    Simplify non-critical block setters by generating height.

--- include/bitcoin/database/query.hpp
+++ include/bitcoin/database/query.hpp
@@ -413,8 +413,7 @@ public:
     bool set(const block& block, const context& ctx,
         bool milestone, bool strong) NOEXCEPT;
     bool set(const transaction& tx) NOEXCEPT;
-    bool set(const block& block, bool strong, bool bypass,
-        size_t height) NOEXCEPT;
+    bool set(const block& block, bool strong, bool bypass) NOEXCEPT;
 
     /// Set transaction.
     code set_code(const transaction& tx) NOEXCEPT;
@@ -440,10 +439,9 @@ public:
         const chain_context& ctx, bool milestone, bool strong) NOEXCEPT;
 
     /// Set block.txs (headers-first).
-    code set_code(const block& block, bool strong, bool bypass,
-        size_t height) NOEXCEPT;
+    code set_code(const block& block, bool strong, bool bypass) NOEXCEPT;
     code set_code(header_link& out_fk, const block& block, bool strong,
-        bool bypass, size_t height) NOEXCEPT;
+        bool bypass) NOEXCEPT;
     code set_code(const block& block, const header_link& key, bool strong,
         bool bypass, size_t height) NOEXCEPT;
 
