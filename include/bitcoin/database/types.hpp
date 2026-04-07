7c0aef65d43e78c98265c77a34156ce59eca7bde
evoskuil <eric@voskuil.org>
2026-04-07 02:15:22 -0400

    Make std::set<history> and std::set<unspent> electrum-sorted.

--- include/bitcoin/database/types.hpp
+++ include/bitcoin/database/types.hpp
@@ -62,11 +62,93 @@ struct fee_rate { size_t bytes{}; uint64_t fee{}; };
 using fee_rates = std::vector<fee_rate>;
 using fee_rate_sets = std::vector<fee_rates>;
 
-struct history { checkpoint tx{}; uint64_t fee{}; };
-using histories = std::set<history>;
+struct base16_less
+{
+    bool operator()(const hash_digest& a, const hash_digest& b) const NOEXCEPT
+    {
+        using namespace system;
+        constexpr auto hi = to_half(byte_bits);
+        constexpr auto lo = sub1(power2<uint8_t>(hi));
 
-struct unspent { outpoint tx{}; size_t height{}; };
-using unspents = std::set<unspent>;
+        for (size_t byte{}; byte < hash_size; ++byte)
+        {
+            const auto hi_a = shift_right(a[byte], hi);
+            const auto hi_b = shift_right(b[byte], hi);
+            if (hi_a != hi_b)
+                return hi_a < hi_b;
+
+            const auto lo_a = bit_and(a[byte], lo);
+            const auto lo_b = bit_and(b[byte], lo);
+            if (lo_a != lo_b)
+                return lo_a < lo_b;
+        }
+
+        return false;
+    }
+};
+
+struct history { checkpoint tx{}; size_t position{}; uint64_t fee{}; };
+struct history_less
+{
+    bool operator()(const history& a, const history& b) const NOEXCEPT
+    {
+        using namespace system;
+        const auto a_height = a.tx.height();
+        const auto b_height = b.tx.height();
+        const auto a_confirmed = !is_min(a_height) && !is_max(a_height);
+        const auto b_confirmed = !is_min(b_height) && !is_max(b_height);
+
+        // Confirmed before unconfirmed.
+        if (a_confirmed != b_confirmed)
+            return a_confirmed;
+
+        // Chain.block height ascending (0 < max | x < y).
+        if (a_height != b_height)
+            return a_height < b_height;
+
+        // Block.tx position ascending (positions must differ).
+        if (a_confirmed)
+            return a.position < b.position;
+
+        // Both unconfirmed (0 or max), base16 lexical txid ascending.
+        return base16_less{}(a.tx.hash(), b.tx.hash());
+    }
+};
+using histories = std::set<history, history_less>;
+
+struct unspent { outpoint tx{}; size_t height{}; size_t position{}; };
+struct unspent_less
+{
+    bool operator()(const unspent& a, const unspent& b) const NOEXCEPT
+    {
+        const auto a_point = a.tx.point();
+        const auto b_point = b.tx.point();
+        const bool a_confirmed = !is_zero(a.height);
+        const bool b_confirmed = !is_zero(b.height);
+
+        // Confirmed before unconfirmed.
+        if (a_confirmed != b_confirmed)
+            return a_confirmed;
+
+        if (a_confirmed)
+        {
+            // Chain.block height ascending (x < y).
+            if (a.height != b.height)
+                return a.height < b.height;
+
+            // Block.tx position ascending.
+            if (a.position != b.position)
+                return a.position < b.position;
+
+            // Tx.output index ascending.
+            return a_point.index() < b_point.index();
+        }
+
+        // Unconfirmed have 0 height and position, arbitrary sort (hash:index).
+        return a_point < b_point;
+    }
+};
+using unspents = std::set<unspent, unspent_less>;
 
 } // namespace database
 } // namespace libbitcoin
