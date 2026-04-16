0a3e5e3f4379c72aadb7042be0bd3dc5cb7f6908
Phillip Mienk <mienkphi@gmail.com>
2026-04-16 08:59:02 -0700

    Regenerate artifacts.

--- builds/cmake/install-presets.sh
+++ builds/cmake/install-presets.sh
@@ -46,14 +46,14 @@ if [[ -z ${secp256k1_TAG} ]]; then
 fi
 
 if [[ -z ${libbitcoin_system_OWNER} ]]; then
-    libbitcoin_system_OWNER="libbitcoin"
+    libbitcoin_system_OWNER="pmienk"
 fi
 if [[ -z ${libbitcoin_system_TAG} ]]; then
     libbitcoin_system_TAG="master"
 fi
 
 if [[ -z ${libbitcoin_database_OWNER} ]]; then
-    libbitcoin_database_OWNER="libbitcoin"
+    libbitcoin_database_OWNER="pmienk"
 fi
 if [[ -z ${libbitcoin_database_TAG} ]]; then
     libbitcoin_database_TAG="master"
@@ -72,6 +72,7 @@ main()
             (--build-full-repositories) BUILD_FULL_REPOSITORIES="yes";;
             (--build-use-local-src)     BUILD_USE_LOCAL_SRC="yes";;
             (--build-parallel=*)        PARALLEL="${OPTION#*=}";;
+            (--build-skip-tests)        BUILD_SKIP_TESTS="yes";;
             (--verbose)                 DISPLAY_VERBOSE="yes";;
             (--help|-h)                 DISPLAY_HELP="yes";;
             (-DCMAKE_PREFIX_PATH=*)     CMAKE_PREFIX_PATH="${OPTION#*=}";;
@@ -323,8 +324,7 @@ main()
             msg_verbose "Exporting CFLAGS '${CFLAGS}'"
         else
             msg_verbose "CFLAGS initially '${CFLAGS}'"
-            SANITIZED_CFLAGS=$(strip_optimization "$CFLAGS")
-            export CFLAGS="${SANITIZED_CFLAGS} ${BUILD_FLAGS}"
+            export CFLAGS="${CFLAGS} ${BUILD_FLAGS}"
             msg_verbose "CFLAGS modified to '${CFLAGS}'"
         fi
 
@@ -333,8 +333,7 @@ main()
             msg_verbose "Exporting CXXFLAGS '${CXXFLAGS}'"
         else
             msg_verbose "CXXFLAGS initially '${CXXFLAGS}'"
-            SANITIZED_CXXFLAGS=$(strip_optimization "$CXXFLAGS")
-            export CXXFLAGS="${SANITIZED_CXXFLAGS} ${BUILD_FLAGS}"
+            export CXXFLAGS="${CXXFLAGS} ${BUILD_FLAGS}"
             msg_verbose "CXXFLAGS modified to '${CXXFLAGS}'"
         fi
     fi
@@ -456,7 +455,9 @@ main()
     local SAVE_CPPFLAGS="${CPPFLAGS}"
     export CPPFLAGS="${CPPFLAGS} ${libbitcoin_database_FLAGS[@]}"
     build_preset "libbitcoin-database" "builds/cmake" "${PARALLEL}" "${libbitcoin_database_OPTIONS[@]}" "${CONFIGURE_OPTIONS[@]}"
-    test_make "libbitcoin-database" "test" "${PARALLEL}"
+    if ! [[ "${BUILD_SKIP_TESTS}" == "yes" ]]; then
+        test_make "libbitcoin-database" "test" "${PARALLEL}"
+    fi
     install_make "libbitcoin-database"
     export CPPFLAGS="${SAVE_CPPFLAGS}"
 
@@ -819,6 +820,7 @@ display_build_variables()
     msg "BUILD_LINK                      : ${BUILD_LINK}"
     msg "BUILD_FULL_REPOSITORIES         : ${BUILD_FULL_REPOSITORIES}"
     msg "BUILD_USE_LOCAL_SRC             : ${BUILD_USE_LOCAL_SRC}"
+    msg "BUILD_SKIP_TESTS                : ${BUILD_SKIP_TESTS}"
     msg "PARALLEL                        : ${PARALLEL}"
     msg "PREFIX                          : ${PREFIX}"
     msg "DISPLAY_VERBOSE                 : ${DISPLAY_VERBOSE}"
@@ -901,16 +903,6 @@ enable_exit_on_error()
 disable_exit_on_error()
 {
     eval "${OPTS_DISABLE}"
-}
-
-strip_optimization()
-{
-    echo "$1" | sed -E '
-        s/-O([0-3]|s|fast|g|z|size|speed)?b?/ /g
-        s/-g([0-3]|gdb|dwarf[0-9]*)?b?/ /g
-        s/[[:space:]]+/ /g
-        s/^ | $//g
-    '
 }
 
 create_directory()
