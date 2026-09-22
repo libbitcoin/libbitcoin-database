5b75e898d44bc977b577ff4d4d1e8d29328d7621
Eric Voskuil <eric@voskuil.org>
2026-09-22 17:09:18 -0400

    Remove dead code.

--- builds/msvc/build/nuget_all.bat
+++ builds/msvc/build/nuget_all.bat
@@ -1,15 +1,5 @@
 @ECHO OFF
-ECHO Downloading libbitcoin vs2022 dependencies from NuGet
-CALL nuget.exe install ..\vs2022\libbitcoin-database\packages.config
-CALL nuget.exe install ..\vs2022\libbitcoin-database-tools\packages.config
-CALL nuget.exe install ..\vs2022\libbitcoin-database-test\packages.config
-ECHO.
-ECHO Downloading libbitcoin vs2019 dependencies from NuGet
-CALL nuget.exe install ..\vs2019\libbitcoin-database\packages.config
-CALL nuget.exe install ..\vs2019\libbitcoin-database-tools\packages.config
-CALL nuget.exe install ..\vs2019\libbitcoin-database-test\packages.config
-ECHO.
-ECHO Downloading libbitcoin vs2017 dependencies from NuGet
-CALL nuget.exe install ..\vs2017\libbitcoin-database\packages.config
-CALL nuget.exe install ..\vs2017\libbitcoin-database-tools\packages.config
-CALL nuget.exe install ..\vs2017\libbitcoin-database-test\packages.config
+ECHO Downloading libbitcoin vs2026 dependencies from NuGet
+CALL nuget.exe install ..\vs2026\libbitcoin-database\packages.config
+CALL nuget.exe install ..\vs2026\libbitcoin-database-tools\packages.config
+CALL nuget.exe install ..\vs2026\libbitcoin-database-test\packages.config
